53d1b44971022c33249fefd7c485bd407ffe6a52
Elijah <elijahreichart@gmail.com>
2024-11-13 13:18:02 -0500

    isolating to test x

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 
-#define in1 2
-#define in2 4
-#define motorPin 5
+#define in11 2
+#define in21 4
+#define motorPin1 5
+// #define in12
+// #define in22
+// #define motorPin2 
 #define xPin 27
 #define yPin 26
 
@@ -10,20 +13,20 @@
 bool direction = true;
 
 void setup() {
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(motorPin, OUTPUT);
+    pinMode(in11, OUTPUT);
+    pinMode(in21, OUTPUT);
+    pinMode(motorPin1, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop() {
     // Read joystick values on X and Y axes
     int x = analogRead(xPin);
-    int y = analogRead(yPin);
+    // int y = analogRead(yPin);
 
     // Map the joystick X value to a motor speed range (0 to 255)
     int xMapped = map(abs(x - 512), 0, 1024, 0, 255); // Normalize X around center 2048
-    int yMapped = map(abs(y - 512), 0, 1024, 0, 255);
+    // int yMapped = map(abs(y - 512), 0, 1024, 0, 255);
 
         if (xMapped > -17 && xMapped < 17) {
         xMapped = 0; // Set to 0 for neutral position
@@ -42,10 +45,10 @@ void loop() {
     Serial.println(direction);
 
     // Set motor direction based on the joystick input
-    digitalWrite(in1, direction);
-    digitalWrite(in2, !direction);
+    digitalWrite(in11, direction);
+    digitalWrite(in21, !direction);
 
     // Set motor speed with PWM based on mapped X value
-    analogWrite(motorPin, xMapped);
+    analogWrite(motorPin1, xMapped);
     delay(100); // small delay to smooth motor response
 }
